296ea745381deeb4ab229be57c4de3d9f226a802
David Tang <dtang859@gmail.com>
2020-11-05 18:38:34 -0800

    2. Query and render DVD titles

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\DvdTitle;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    $dvdTitles = DvdTitle::all();
+
+    return view('index', [
+        'dvds' => $dvdTitles
+    ]);
 });
